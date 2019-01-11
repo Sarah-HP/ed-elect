59aba1ca8bc5b04892a54a2e8a15e915b74ee337
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-11 16:51:22 -0500

    add Tweet text and other info

--- t_scraper.py
+++ t_scraper.py
@@ -6,8 +6,6 @@ consumer_secret="s5uGQVieitvw5CT5FVBj7yM08aP84zf8HNpm4Ya99RtxsTnBA2"
 access_token="1083483578359599104-iesJJeVOjPKmTB4peDWVIWzIJCl0rU"
 access_token_secret="QREgbBttdMhWRCE7xBtXAIzNN0tG65QsvIstMxmgjcrt3"
 
-
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
@@ -18,14 +16,15 @@ user_id=357606935
 #Find ID of most recent Tweet:
 first_tweet=api.user_timeline(user_id,count=1)
 
-min_id=first_tweet[0].id+1
+#set first max ID number for Tweets:
+max_id=first_tweet[0].id+1
 
+#Iterate through all Tweets:
 for i in range(29):
-	poli_tweets = api.user_timeline(user_id, max_id=(min_id-1), count=100, tweet_mode="extended")
+	poli_tweets = api.user_timeline(user_id, max_id=(max_id-1), count=100, tweet_mode="extended")
 	for tweet in poli_tweets:
-		print(tweet.id)
-	min_id=poli_tweets[-1].id
-
+		print(str(tweet.id) + ", " + str(tweet.created_at) + ", " + tweet.full_text + ", " + str(tweet.place) + ", " + str(tweet.retweet_count) + ", " + str(tweet.favorite_count))
+	max_id=poli_tweets[-1].id
 
 #for tweet in poli_tweets:
 #	print(str(tweet.id) + ", " + str(tweet.created_at) + ", " + tweet.full_text + ", " + str(tweet.place) + ", " + str(tweet.retweet_count) + ", " + str(tweet.favorite_count))

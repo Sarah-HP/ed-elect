8ac4482e49fd4879861a9c117bfb7d4b286340e5
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-11 15:50:56 -0500

    make output in csv format

--- t_scraper.py
+++ t_scraper.py
@@ -15,4 +15,4 @@ api = tweepy.API(auth)
 
 poli_tweets = api.user_timeline(357606935, count=3000, tweet_mode="extended")
 for tweet in poli_tweets:
-    print(tweet.created_at, tweet.full_text)
+    print(str(tweet.created_at) + ", " + tweet.full_text)

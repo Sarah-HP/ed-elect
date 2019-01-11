b083e7a4a1549fe8869766974397ac74aa5ea0fd
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-11 17:32:14 -0500

    make scraper output CSV

--- scraping/t_scraper.py
+++ scraping/t_scraper.py
@@ -1,4 +1,5 @@
 import tweepy
+import csv
 
 consumer_key="jJevgPoZqCvvlaEaDfCjSo3T4"
 consumer_secret="s5uGQVieitvw5CT5FVBj7yM08aP84zf8HNpm4Ya99RtxsTnBA2"
@@ -19,25 +20,13 @@ first_tweet=api.user_timeline(user_id,count=1)
 #set first max ID number for Tweets:
 max_id=first_tweet[0].id+1
 
-#Iterate through all Tweets:
-for i in range(29):
-	poli_tweets = api.user_timeline(user_id, max_id=(max_id-1), count=100, tweet_mode="extended")
-	for tweet in poli_tweets:
-		print(str(tweet.id) + ", " + str(tweet.created_at) + ", " + tweet.full_text + ", " + str(tweet.place) + ", " + str(tweet.retweet_count) + ", " + str(tweet.favorite_count))
-	max_id=poli_tweets[-1].id
+file_name = str(user_id)+"_tweets.csv"
 
-#for tweet in poli_tweets:
-#	print(str(tweet.id) + ", " + str(tweet.created_at) + ", " + tweet.full_text + ", " + str(tweet.place) + ", " + str(tweet.retweet_count) + ", " + str(tweet.favorite_count))
-
-#print("THIS IS THE GAP BETWEEN THEM")
-
-#for tweet in poli_tweets2:
-#	print(tweet.id_str + ", " + str(tweet.created_at) + ", " + tweet.full_text + ", " + str(tweet.place) + ", " + str(tweet.retweet_count) + ", " + str(tweet.favorite_count))
-
-
-#check to see if education comes up much
-#for tweet in poli_tweets:
-#	if "education" in tweet.full_text:
-#		print(str(tweet.created_at) + ", " + tweet.full_text + ", " + str(tweet.place) + ", " + str(tweet.retweet_count) + ", " + str(tweet.favorite_count))
-#	else:
-#		print("nah")
+with open(file_name, 'w', newline='') as csvfile:
+	csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+	#Iterate through all Tweets:
+	for i in range(29):
+		poli_tweets = api.user_timeline(user_id, max_id=(max_id-1), count=100, tweet_mode="extended")
+		for tweet in poli_tweets:
+			csvwriter.writerow([tweet.id]+[tweet.created_at]+[tweet.full_text]+[tweet.place]+[tweet.retweet_count]+[tweet.favorite_count])
+		max_id=poli_tweets[-1].id

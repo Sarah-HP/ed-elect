d1bd8978338e3299b5e00f040744fc566901c3c7
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-11 18:48:21 -0500

    make scraper run from command line, start scraping, compile Twitters

--- scraping/t_scraper.py
+++ scraping/t_scraper.py
@@ -1,5 +1,16 @@
+#!/usr/bin/env python3
+
 import tweepy
 import csv
+import argparse
+
+parser=argparse.ArgumentParser(description="make name and ID # usable for scraper file")
+parser.add_argument("cand_name")
+parser.add_argument("user_id",type=int)
+args=parser.parse_args()
+
+cand_name=args.cand_name
+user_id=args.user_id
 
 consumer_key="jJevgPoZqCvvlaEaDfCjSo3T4"
 consumer_secret="s5uGQVieitvw5CT5FVBj7yM08aP84zf8HNpm4Ya99RtxsTnBA2"
@@ -12,7 +23,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-user_id=33537967
+#user_id=33537967
 
 #Find ID of most recent Tweet:
 first_tweet=api.user_timeline(user_id,count=1)
@@ -20,13 +31,15 @@ first_tweet=api.user_timeline(user_id,count=1)
 #set first max ID number for Tweets:
 max_id=first_tweet[0].id+1
 
-file_name = str(user_id)+"_tweets.csv"
+file_name = cand_name+"_tweets.csv"
 
 with open(file_name, 'w', newline='') as csvfile:
 	csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 	#Iterate through all Tweets:
-	for i in range(50):
+	poli_tweets=[1,2]
+	while len(poli_tweets) > 0:
 		poli_tweets = api.user_timeline(user_id, max_id=(max_id-1), count=100, tweet_mode="extended")
+#		print(len(poli_tweets))
 		for tweet in poli_tweets:
 			csvwriter.writerow([tweet.id]+[tweet.created_at]+[tweet.full_text]+[tweet.place]+[tweet.retweet_count]+[tweet.favorite_count])
-		max_id=poli_tweets[-1].id
+			max_id=tweet.id

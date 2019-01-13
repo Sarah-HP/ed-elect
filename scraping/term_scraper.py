3c2a887f3c5f4a0f790a6f1142594fcea090ae75
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-13 13:21:45 -0500

    remove useless date argument

--- scraping/term_scraper.py
+++ scraping/term_scraper.py
@@ -2,14 +2,12 @@
 
 
 # Run this search directly from the command line. Input format:
-# ./term_scraper.py "[search term]" "[start data--format: YYYY-MM-DD]"
-# Put quotation marks around both those inputs
+# ./term_scraper.py "[search term]"
+# Put quotation marks around the search term
 # Example input:
-# ./term_scraper.py "#potentialgrizzlies" "2019-01-01"
+# ./term_scraper.py "#potentialgrizzlies"
 
 #Results print to term_search_results folder
-
-
 
 import tweepy
 import csv
@@ -17,11 +15,9 @@ import argparse
 
 parser=argparse.ArgumentParser(description="scrape Twitter for terms")
 parser.add_argument("search_term")
-parser.add_argument("start_date")
 args=parser.parse_args()
 
 search_for=args.search_term
-start_date=args.start_date
 
 consumer_key="jJevgPoZqCvvlaEaDfCjSo3T4"
 consumer_secret="s5uGQVieitvw5CT5FVBj7yM08aP84zf8HNpm4Ya99RtxsTnBA2"
@@ -34,13 +30,14 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-file_name= "term_search_results/results_for_" + search_for + ".csv"
+start_date = "2016-01-01"
+file_name = "term_search_results/results_for_" + search_for + ".csv"
 
 with open(file_name, 'w', newline='') as csvfile:
 	csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 	#Iterate through all Tweets:
 #	poli_tweets=[1,2]
 #	while len(poli_tweets) > 0:
-	poli_tweets = tweepy.Cursor(api.search,q=search_for, count = 100, tweet_mode="extended", since=start_date).items()
+	poli_tweets = tweepy.Cursor(api.search,q=search_for, count = 100, tweet_mode="extended", since=start_date, wait_on_rate_limit=True, wait_on_rate_limit_notify=True).items()
 	for tweet in poli_tweets:
 		csvwriter.writerow([tweet.user.name]+[tweet.user.location]+[tweet.user.verified]+[tweet.id]+[tweet.created_at]+[tweet.full_text]+[tweet.place]+[tweet.retweet_count]+[tweet.favorite_count])

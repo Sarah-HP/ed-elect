424afe830e8b558e4291761123c6f66dc15bb0f3
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-14 01:33:32 -0500

    add plots about cand tweets, add R code used to make them

--- scraping/Tweet_analysis_R_code.R
+++ scraping/Tweet_analysis_R_code.R
@@ -13,12 +13,19 @@ HE_tweets<-filter(edu_tweets,grepl('higher education|college',Tweet.Text,ignore.
 gun_tweets<-filter(edu_tweets,grepl('NRA|gun|shoot|March for our lives|Newtown|sandy hook',Tweet.Text,ignore.case = TRUE))
 
 
+e_count<- as.data.frame(table(edu_tweets$Candidate))
+e_count<-mutate(e_count,ed_t_count = Freq)
+e_count<-select(e_count,-Freq)
+t_count <- as.data.frame(table(tweets$Candidate))
+t_count<-mutate(t_count,t_count = Freq)
+t_count<-select(t_count,-Freq)
+totals<-left_join(e_count,t_count,by="Var1")
+totals<-mutate(totals,ed_perc = ed_t_count/t_count)
 
 
 
-
-
-
+ggplot(totals,aes(Var1,ed_perc))+geom_col()+theme(axis.text.x=element_text(angle = 90))+labs(title="Percent of Candidate Tweets Containing Terms Related to Education",x="Candidate Name", y="Percent of Teeets Containing Education-\nRelated Terms")
+ggplot(totals,aes(reorder(Var1,-ed_perc,sum),ed_perc))+geom_col()+theme(axis.text.x=element_text(angle = 90))+labs(title="Percent of Candidate Tweets Containing Terms Related to Education",x="Candidate Name", y="Percent of Teeets Containing Education-\nRelated Terms")
 
 
 

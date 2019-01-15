9ee82cdeb2bd7ed7cc4981d8ada918e8e3b1ea49
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-15 17:23:49 -0500

    make tweets be input, add comments

--- scraping/common_words.py
+++ scraping/common_words.py
@@ -1,40 +1,36 @@
 import csv
 
 #read in list of all candidate tweets
-#with open('aggregated.csv', 'r') as f:
-#	reader=csv.DictReader(f)
-#	rows = list(reader)
+with open('aggregated.csv', 'r') as f:
+	reader=csv.DictReader(f)
+	rows = list(reader)
 
-#read in the tweets
-#tweets=[]
-#for row in rows:
-#	item=dict(row)
-#	tweets.append(item)
+#read in the tweets as a dictionary
+tweets=[]
+for row in rows:
+	item=dict(row)
+	tweets.append(item)
 
 #make a list with just the tweets' text
-#text = []
+text = []
+for tweet in tweets:
+	text.append(tweet[' Tweet Text'])
 
-#for tweet in testtw:
-#	text.append(tweet[' Tweet Text'])
-
-
-#testing with simpler list of strings
-text = ['This is a sentence', 'This is too', 'So is this one']
-
+#make a list of just the words
 words = []
-
 for i in text:
+	#make all lowercase
 	lower_i = i.lower()
+	#split each tweet into list of words
 	lower_i_split = lower_i.split()
+	#loop through word lists
 	for j in lower_i_split:
 		words.append(j)
 
+#make dictionary of word counts
 word_count = {}
-
 for word in words:
 	if word in word_count:
 		word_count[word] += 1
 	else:
 		word_count[word] = 1
-
-print(word_count)

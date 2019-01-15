9a4231fedf4426a942cd9898c289993d8f636a54
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-15 17:15:25 -0500

    write code to look for common words in Tweets

---
+++ scraping/common_words.py
@@ -0,0 +1,32 @@
+import csv
+
+#read in list of all candidate tweets
+#with open('aggregated.csv', 'r') as f:
+#	reader=csv.DictReader(f)
+#	rows = list(reader)
+
+#read in the tweets
+#tweets=[]
+#for row in rows:
+#	item=dict(row)
+#	tweets.append(item)
+
+#make a list with just the tweets' text
+#text = []
+
+#for tweet in testtw:
+#	text.append(tweet[' Tweet Text'])
+
+
+#testing with simpler list of strings
+text = ['This is a sentence', 'This is too', 'So is this one']
+
+words = []
+
+for i in text:
+	lower_i = i.lower()
+	lower_i_split = lower_i.split()
+	for j in lower_i_split:
+		words.append(j)
+
+print(words)

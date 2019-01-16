8ffddc25c08beb271dbc1bd04ebdffe0822a68f5
samhoran <rhoran@jd20.law.harvard.edu>
2019-01-16 18:20:59 -0500

    modify filter to output tsv of educ tweet perc

--- scraping/tweet_filter.py
+++ scraping/tweet_filter.py
@@ -90,6 +90,15 @@ for tweet in edu_tweets:
 	else:
 		cand_edu_tweet_count[cand] = 1
 
+#Count total number of tweets by candidate
+cand_tweet_count = {}
+for tweet in tweets:
+	cand = tweet['Candidate']
+	if cand in cand_tweet_count:
+		cand_tweet_count[cand] += 1
+	else:
+		cand_tweet_count[cand] = 1
+
 #Create list of lists with list for each candidate with # of aff tweets, # of edu tweets, and # of total tweets
 #First make list of candidates - there are more efficient ways of doing this, but just in case there's one with no tweets about edu or something I'll use the complete list of aggregated tweets:
 cand_list=[]
@@ -105,14 +114,26 @@ for i in cand_list:
 		cand_edu_tweet_count[i] = 0
 	if i not in cand_coll_aff_count:
 		cand_coll_aff_count[i] = 0
+
 #now pull together cand name, edu tweet count, and college aff tweet count
 for i in cand_list:
-	cand_summary.append([i, cand_edu_tweet_count[i], cand_coll_aff_count[i]])
-
+	cand_summary.append([i, cand_tweet_count[i], cand_edu_tweet_count[i], cand_coll_aff_count[i]])
 
 #Write candidate summary to a CSV
 with open('edu_tweet_counts_by_cand.csv','w') as f:
     writer = csv.writer(f)
-    writer.writerow(['candidate','education_tweets','college_affordability_tweets'])
+    writer.writerow(['candidate','tweets','education_tweets','college_affordability_tweets'])
     for j in cand_summary:
         writer.writerow(j)
+
+#Create list with education tweet % for each candidate:
+edu_tweet_perc = []
+for i in cand_summary:
+	edu_tweet_perc.append([i[0],i[2]/i[1]])
+
+#Write percentage of educational tweets to tsv as data for plot of candidate tweet %
+with open ('../data/cand_2020.tsv','w') as f:
+	tsv_writer = csv.writer(f, delimiter = '\t')
+	tsv_writer.writerow(['candidate', 'ed_perc'])
+	for i in edu_tweet_perc:
+		tsv_writer.writerow(i)

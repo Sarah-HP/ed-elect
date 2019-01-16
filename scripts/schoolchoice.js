da4a5bbf28c251ba6644f57eba66d5e87d30d628
SARAH POLLNOW <SPOLLNOW@GSE.HARVARD.EDU>
2019-01-15 21:25:25 -0500

    Split the script up so it looks tidier

--- scripts/schoolchoice.js
+++ scripts/schoolchoice.js
@@ -1,3 +1,12 @@
 
 //Embed the script for the interactive map of the U.S.
-type="text/javascript">if("undefined"==typeof window.datawrapper)window.datawrapper={};window.datawrapper["ZcIEN"]={},window.datawrapper["ZcIEN"].iframe=document.getElementById("datawrapper-chart-ZcIEN"),window.addEventListener("message",function(a){if("undefined"!=typeof a.data["datawrapper-height"])for(var b in a.data["datawrapper-height"])if("ZcIEN"==b)window.datawrapper["ZcIEN"].iframe.style.height=a.data["datawrapper-height"][b]+"px"});
+type="text/javascript">
+	if("undefined"==typeof window.datawrapper)
+		window.datawrapper={};window.datawrapper["ZcIEN"]={},
+		window.datawrapper["ZcIEN"].iframe=
+		document.getElementById("datawrapper-chart-ZcIEN"),
+		window.addEventListener("message",function(a)
+			{if("undefined"!=typeof a.data["datawrapper-height"])
+			for(var b in a.data["datawrapper-height"])
+				if("ZcIEN"==b)window.datawrapper["ZcIEN"].iframe.style.
+					height=a.data["datawrapper-height"][b]+"px"});
